7b7009838ddce0a23020d4cde1709927f738dc1f
Chenghao Lyu <chenghao@cs.umass.edu>
2023-10-27 15:56:05 +0200

    update

--- benchmark-run/script-tpch/run-taskb-adapt.sh
+++ benchmark-run/script-tpch/run-taskb-adapt.sh
@@ -3,13 +3,16 @@
 tid=${1:-9}
 qid=${2:-1}
 
-s1s4s5Default="64MB 10MB 200"
-s1s4s5Worst1="256MB 10MB 200"
-s1s4s5Worst2="256MB 320MB 200"
-s1s4s5Worst3="64MB 320MB 200"
+s1s4s5_cand1="16MB 10MB 100"
+s1s4s5_cand2="64MB 10MB 200"
+s1s4s5_cand3="256MB 10MB 200"
 
-for s1s4s5 in "$s1s4s5Worst1" "$s1s4s5Worst2" "$s1s4s5Worst3" "$s1s4s5Default"; do
-  for uId in 1 4; do
+# s1s4s5Worst1="256MB 10MB 100"
+# s1s4s5Worst2="16MB 320MB 100"
+# s1s4s5Worst3="16MB 10MB 500"
+
+for s1s4s5 in "$s1s4s5_cand1" "$s1s4s5_cand2" "$s1s4s5_cand3"; do
+  for uId in 1; do
     read s1 s4 s5 <<< $s1s4s5
     name=tpch100_q${tid}-${qid}_s1:${s1}_s4:${s4}_s5:${s5}_adapt_uId:${uId}
     for i in 1 2 3; do
---
+++ benchmark-run/script-tpch/run-taskc-adapt.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+tid=${1:-9}
+qid=${2:-1}
+
+s1s4s5="16MB 10MB 100"
+read s1 s4 s5 <<< $s1s4s5
+
+for k1 in 8g 16g 32g; do
+  for k2 in 3 5 ; do
+    for k3 in 4 6 8 10; do
+      for i in 1 2 3; do
+        name=tpch100_q${tid}-${qid}_k1:${k1}_k2:${k2}_k3:${k3}_over_adapt
+        sync
+        ssh node2 sync
+        ssh node3 sync
+        ssh node4 sync
+        ssh node5 sync
+        ssh node6 sync
+        sleep 5
+        echo -------------------------
+        echo start running $name x$i
+        bash run-tpch-q-adapt.sh -n $name -q "${tid} ${qid}" -c "${k1} ${k2} ${k3} $((k2 * k3 * 2)) 48m 200 true 0.6" -p "${s1} 0.2 0b ${s4} ${s5} 256MB 5.0" -x "outs/taskc" | tee logs/${name}_x${i}.log 2>&1
+      done
+    done
+  done
+done
