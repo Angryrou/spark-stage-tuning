6774df6dd3b00ea62cf07b70f814eca4553b9478
Chenghao Lyu <chenghao@cs.umass.edu>
2025-05-04 20:30:53 -0400

    dupate

--- src/main/scala/edu/polytechnique/cedar/spark/benchmark/RunJOBTemplateQueryForRuntime.scala
+++ src/main/scala/edu/polytechnique/cedar/spark/benchmark/RunJOBTemplateQueryForRuntime.scala
@@ -57,7 +57,7 @@ object RunJOBTemplateQueryForRuntime {
   }
 
   def run(config: RunJOBTemplateQueryConfig): Unit = {
-    assert(config.benchmarkName.startsWith("job-"))
+    assert(config.benchmarkName.startsWith("job-") or config.benchmarkName.startsWith("ceb-imdb"))
     val tid: String = config.templateName
     val collector = new UdaoCollector(config.verbose, tid)
     val udaoClient: Option[UdaoClient] =
